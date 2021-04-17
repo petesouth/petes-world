75e901b7cccba5977307a2dccfb1f64888c09a44
petesouthwell <pete@issarainstitute.org>
2021-04-17 23:18:06 +0000

    First bootstraping this thing... Petes-world.. YAY

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {process.env.REACT_APP_OPENING_MESSAGE}
         </a>
       </header>
     </div>
